4497f2c212de5e4f239e63edb41d8ea6f8e06449
DannyTrocchia <124284119+DannyTrocchia@users.noreply.github.com>
2023-02-06 11:05:26 -0500

    A sample program that inputs, calculates and outputs data

---
+++ test.py
@@ -0,0 +1,12 @@
+#This program inputs a name and outputs the name
+name = input("Enter your name: ")
+dob = input("Enter your birthdate for example December 20, 1999:")
+print ("Your name is",name)
+print ("Your bithdate is",dob)
+
+a = int(input("Enter the value for a:"))
+b = int(input("Enter the value for b:"))
+c = a + b
+print("the total is:",c)
+
+
